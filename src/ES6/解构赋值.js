c21181107a35abd58cf1fe7e07037e50d4e31512
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-08-30 08:49:22 +0800

    解构赋值

--- src/ES6/解构赋值.js
+++ src/ES6/解构赋值.js
@@ -1,10 +1,10 @@
-// 基本用法  Destructuring
+// 1. 基本用法  Destructuring
 
 let [ a, b, c ] = [1, 2, 3];
 
-let { d,e }={ d:1, e:2 }
+let { d,e }={ d:1, e:2 };
 
-// 深度解构 deep destructuring
+// 2. 深度解构 deep destructuring
 let obj = {
   p: [
     'Hello',
@@ -21,12 +21,12 @@ function add([x, y]){
 add([1, 2]); // 3
 
 
-//字符串的解构赋值 
+// 3. 字符串的解构赋值
 const [a1, b1, c1, d1, e1] = 'hello';
 
-//rest解构 spread destructuring
+// 4 .rest解构 spread destructuring
 const { p,...rest } = {p:1,a:2,c:2};
-console.log(rest) // {a:2, c:2}
+console.log(rest); // {a:2, c:2}
 // 默认值 ,如果一个数组成员不严格等于undefined
 
 // //函数参数的解构赋值
