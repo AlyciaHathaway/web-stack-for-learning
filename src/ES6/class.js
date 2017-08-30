61cd55fef56159583c91f82e24125bef20da421f
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-08-30 22:38:21 +0800

    class

--- src/ES6/class.js
+++ src/ES6/class.js
@@ -1,28 +1,33 @@
 class TestSuper {
 	constructor() {
-		this.c = 'a1';
+		this.a = 'a1'
 	}
-	e(){}
+	d() {}
 }
-class Test extends TestSuper{ 
-	constructor(){
+
+class Test extends TestSuper{
+	constructor() {
 		super();
 		this.a = 'a';
 		this.b = 'b'
 	}
-	d(){
+	c() {
 		return 'ddddd';
 	}
 }
 
 let ins = new Test();
-// React用class语法来写
+console.log(ins);
+// console.log(ins.prototype.c);
 
+// 上下等价，class是更加结构化的语法
+// 如果存在继承关系，要手动call一下super
+// react也是用class语法来写
 
-let TestFactory = function (argument) {
+let TestFactory = function(argument) {
 	this.a = 'a';
 	this.b = 'b'
 };
-Test.prototype.c = () => 'ddddd';
+Test.prototype.c = ()=> 'ddddd';
 let inst = new Test();
-console.log(inst.d)
+console.log(inst.c);
